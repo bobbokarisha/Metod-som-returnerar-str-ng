375eea995c25c55a4b2a735878695e4771dac749
dinho <dinho@LAPTOP-D7O6TG3O>
2023-12-29 14:25:38 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+string input = "snake";
+
+Console.WriteLine(input);
+Console.WriteLine(ReverseWord(input));
+
+string ReverseWord(string word) // Hur blir word = string input = "snake"; ? borde det inte stå (string input) i metoden?
+{
+    string result = "";
+    for (int i = word.Length - 1; i >= 0; i--)
+    {
+        result += word[i];
+    }
+    return result;
+}
